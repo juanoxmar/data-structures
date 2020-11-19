568e3890eb02958721a5890b76d9846bf9db3f98
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-11-19 15:52:22 -0800

    Completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    const newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    const oldHead = list.head.value;
+    if (!list.head.next) {
+      list.head = null;
+      list.tail = null;
+    } else {
+      list.head = list.head.next;
+    }
+    return oldHead;
   };
 
   list.contains = function(target) {
+    let currentNode = list.head;
+
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +51,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail: O(1)
+ * removeHead: O(1)
+ * contains: O(n)
  */
