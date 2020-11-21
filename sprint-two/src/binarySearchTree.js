b893d273187419be0747fe315c8baa1722addeea
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-11-21 10:23:01 -0800

    Changed depthFirstLog time complexity to O(n)

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -70,5 +70,5 @@ binaryTreeMethods.depthFirstLog = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  * insert: O(log(n))
  * contains: O(log(n))
- * depthFirstLog: O(log(n))
+ * depthFirstLog: O(n)
  */
